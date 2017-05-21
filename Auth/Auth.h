72f998f2cbabd81fafb9a6bddd14dfab6b8f26ec
asharijuang <asharijuang@me.com>
2017-05-21 21:43:07 +0700

    Auth framework

---
+++ Auth/Auth.h
@@ -0,0 +1,19 @@
+//
+//  Auth.h
+//  Auth
+//
+//  Created by Qiscus on 5/21/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Auth.
+FOUNDATION_EXPORT double AuthVersionNumber;
+
+//! Project version string for Auth.
+FOUNDATION_EXPORT const unsigned char AuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Auth/PublicHeader.h>
+
+
