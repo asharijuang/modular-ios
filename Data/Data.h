466a79d0a1ea39932ecfee309474bc377e60b671
asharijuang <asharijuang@me.com>
2017-05-21 21:42:26 +0700

    initiate data framework

---
+++ Data/Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Qiscus on 5/21/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
