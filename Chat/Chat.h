79bc9ac48c6766f60c7bc28f7b8d509d8356853f
asharijuang <asharijuang@me.com>
2017-05-21 21:40:32 +0700

    initiate Chat framework

---
+++ Chat/Chat.h
@@ -0,0 +1,19 @@
+//
+//  Chat.h
+//  Chat
+//
+//  Created by Qiscus on 5/21/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chat.
+FOUNDATION_EXPORT double ChatVersionNumber;
+
+//! Project version string for Chat.
+FOUNDATION_EXPORT const unsigned char ChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chat/PublicHeader.h>
+
+
