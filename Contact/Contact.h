b1be4e812824a7e42a90eef2f6a9102c0e3f4b10
asharijuang <asharijuang@me.com>
2017-05-21 21:41:38 +0700

    initiate Contact Framework

---
+++ Contact/Contact.h
@@ -0,0 +1,19 @@
+//
+//  Contact.h
+//  Contact
+//
+//  Created by Qiscus on 5/21/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Contact.
+FOUNDATION_EXPORT double ContactVersionNumber;
+
+//! Project version string for Contact.
+FOUNDATION_EXPORT const unsigned char ContactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Contact/PublicHeader.h>
+
+
